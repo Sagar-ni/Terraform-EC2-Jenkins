f669157d2993dd941dabc78d4ead5234f24fb3df
Jackson Moris <161938805+Sagar-ni@users.noreply.github.com>
2024-03-30 15:54:58 +0530

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "jenkinsserver" {
   instance_type               = var.instance_type
   key_name                    = "Ganesh"
   subnet_id                   = aws_subnet.public_subnet.id
-  availability_zone           = data.aws_availability_zones.available_1.names[0]
+  availability_zone           = var.avail_zone
   associate_public_ip_address = true
   vpc_security_group_ids      = aws_security_group.Mysg.id
   user_data                   = file("jenkins-server-script.sh")
