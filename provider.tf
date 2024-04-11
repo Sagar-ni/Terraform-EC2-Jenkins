55deb0a4f8c25f7ae932412a1d0f148f38857377
Jackson Moris <161938805+Sagar-ni@users.noreply.github.com>
2024-04-11 14:32:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,15 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
+      version = "5.4.0"
     }
   }
 }
 
 
-
-
 provider "aws" {
-  region = "us-east-1"
-
+  region = "ap-south-1"
 }
+
+
