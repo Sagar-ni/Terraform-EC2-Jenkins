9ae5a7a007eb44db47bd74e015acfd3e1b9a42e9
Jackson Moris <161938805+Sagar-ni@users.noreply.github.com>
2024-04-11 14:33:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
-variable "Env_prefix" {}
-variable "myvpc_cidr" {}
-variable "public_subnet_cidr" {}
+variable "env" {}
+variable "project" {}
+variable "vpc_cidr" {}
+variable "public_subnet_1_cidr" {}
+variable "public_subnet_2_cidr" {}
 variable "instance_type" {}
-variable "avail_zone" {}
+
+
+
+
